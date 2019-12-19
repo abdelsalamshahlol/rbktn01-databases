8f404281bdf291d109849e38e91bf4e236bdafd1
abdelsalamshahlol <abdulsalamshahlol@gmail.com>
2019-12-19 13:44:55 +0100

    schema complete

--- server/schema.sql
+++ server/schema.sql
@@ -2,16 +2,55 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
 
-/* Create other tables and define schemas for them here! */
+DROP TABLE IF EXISTS `users`;
 
+CREATE TABLE `users` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) COMMENT 'table for chatterbox users';
 
+-- ---
+-- Table 'messages'
+-- table for chatterbox message
+-- ---
 
+DROP TABLE IF EXISTS `messages`;
 
-/*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
+CREATE TABLE `messages` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `content` MEDIUMTEXT NOT NULL,
+  `id_user` INTEGER NOT NULL,
+  `id_room` INTEGER NOT NULL,
+  `created_at` TIMESTAMP NOT NULL,
+  PRIMARY KEY (`id`)
+) COMMENT 'table for chatterbox message';
 
+-- ---
+-- Table 'rooms'
+-- table for rooms on chatterbox
+-- ---
+
+DROP TABLE IF EXISTS `rooms`;
+
+CREATE TABLE `rooms` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) COMMENT 'table for rooms on chatterbox';
+
+-- ---
+-- Foreign Keys
+-- ---
+
+ALTER TABLE `messages` ADD FOREIGN KEY (id_user) REFERENCES `users` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (id_room) REFERENCES `rooms` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
